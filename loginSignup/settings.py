9a37ba9d1c3fcb76c3aaf535fa7cf7314ec03772
Ragnvindr <113169816+ragnvindr08@users.noreply.github.com>
2024-06-18 03:08:24 +0800

    Update settings.py

--- loginSignup/settings.py
+++ loginSignup/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-6)xt=zud5@60vk)wkpdnuo+ch$if04bxa78xlm6yc&bwxuem2v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
